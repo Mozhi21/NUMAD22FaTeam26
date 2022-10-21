27864b1f12f6275be34982b5dcf5e225143d9113
FentPams <xinyifeng94@gmail.com>
2022-10-21 01:29:42 -0700

    Xinyi: Create search activity

--- app/src/main/java/edu/northeastern/numad22fateam26/MainActivity.java
+++ app/src/main/java/edu/northeastern/numad22fateam26/MainActivity.java
@@ -2,13 +2,20 @@ package edu.northeastern.numad22fateam26;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toSearchActivity(View view){
+        startActivity(new Intent(MainActivity.this, SpoonacularSearchActivity.class));
     }
 }
---
+++ app/src/main/java/edu/northeastern/numad22fateam26/SpoonacularSearchActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numad22fateam26;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpoonacularSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spoonacular_search);
+    }
+}
