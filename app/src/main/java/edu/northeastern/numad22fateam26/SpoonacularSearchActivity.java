95959780523847303cc5aedb8d3af28596527755
FentPams <xinyifeng94@gmail.com>
2022-10-21 02:17:09 -0700

    initialize Search Activity

--- app/src/main/java/edu/northeastern/numad22fateam26/SpoonacularSearchActivity.java
+++ app/src/main/java/edu/northeastern/numad22fateam26/SpoonacularSearchActivity.java
@@ -3,6 +3,7 @@ package edu.northeastern.numad22fateam26;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SpoonacularSearchActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class SpoonacularSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spoonacular_search);
+    }
+
+    public void back(View view){
+        SpoonacularSearchActivity.super.onBackPressed();
     }
 }
