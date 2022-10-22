e4dd477c77588770e5536d900c3bbafaaccf3e13
FentPams <xinyifeng94@gmail.com>
2022-10-22 13:50:32 -0700

    Xinyi: Adding slider on Search Activity

--- app/src/main/java/edu/northeastern/numad22fateam26/SpoonacularSearchActivity.java
+++ app/src/main/java/edu/northeastern/numad22fateam26/SpoonacularSearchActivity.java
@@ -1,13 +1,19 @@
 package edu.northeastern.numad22fateam26;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class SpoonacularSearchActivity extends AppCompatActivity {
+import com.google.android.material.slider.LabelFormatter;
+import com.google.android.material.slider.Slider;
 
+import butterknife.BindView;
+
+public class SpoonacularSearchActivity extends AppCompatActivity {
+    Slider slider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
