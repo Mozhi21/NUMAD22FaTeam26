b454db9b2eebd2965df266ae7f3ebc98bab061c5
FentPams <xinyifeng94@gmail.com>
2022-10-22 00:58:25 -0700

    Xinyi:complete search layout and adapater

---
+++ app/src/main/java/edu/northeastern/numad22fateam26/model/Recipe.java
@@ -0,0 +1,47 @@
+package edu.northeastern.numad22fateam26.model;
+
+public class Recipe {
+    int id;
+    String title;
+    String image;
+    String imageType;
+
+    public Recipe(int id, String title, String image, String imageType) {
+        this.id = id;
+        this.title = title;
+        this.image = image;
+        this.imageType = imageType;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getImageType() {
+        return imageType;
+    }
+
+    public void setImageType(String imageType) {
+        this.imageType = imageType;
+    }
+}
