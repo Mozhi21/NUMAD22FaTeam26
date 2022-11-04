d0daefc6b7e714d9dc7f4bbfee452594669bd8f8
FentPams <xinyifeng94@gmail.com>
2022-11-03 23:13:03 -0700

    create/update user-token in database

--- app/src/main/java/edu/northeastern/numad22fateam26/SignInActivity.java
+++ app/src/main/java/edu/northeastern/numad22fateam26/SignInActivity.java
@@ -17,15 +17,25 @@ import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessaging;
+
+import java.util.Objects;
 
 public class SignInActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
+    private FirebaseDatabase mDatabase;
+    private FirebaseMessaging mMessageing;
     private TextInputLayout usernameInput;
 
     private static String TAG = "SignInActivity";
     private static String DEFAULT_EMAIL_SUFFIX = "@group26.com";
     private static String DEFAULT_PASSWORD = "group26";
+    private static String DATABASE_USERS = "users";
+    private static String DATABASE_USERNAME = "username";
+    private static String DATABASE_FCMToken = "FCMToken";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +43,8 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
 
         mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance();
+        mMessageing = FirebaseMessaging.getInstance();
         usernameInput = findViewById(R.id.username);
     }
 
@@ -48,36 +60,39 @@ public class SignInActivity extends AppCompatActivity {
 
     public void login(View view) {
         String username = usernameInput.getEditText().getText().toString();
-        signIn(username + DEFAULT_EMAIL_SUFFIX);
-
+        signIn(username);
     }
 
-    private void signIn(String email) {
-        mAuth.signInWithEmailAndPassword(email, DEFAULT_PASSWORD)
+    private void signIn(String username) {
+        mAuth.signInWithEmailAndPassword(wrapWithEmailSuffix(username), DEFAULT_PASSWORD)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
+                            String uid = Objects.requireNonNull(task.getResult().getUser()).getUid();
+                            updateUserTokenInDatabase(uid, username);
                             transitionToSocialMediaActivity();
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
-                            signUp(email);
+                            signUp(username);
                         }
                     }
                 });
     }
 
-    private void signUp(String email) {
-        mAuth.createUserWithEmailAndPassword(email, DEFAULT_PASSWORD)
+    private void signUp(String username) {
+        mAuth.createUserWithEmailAndPassword(wrapWithEmailSuffix(username), DEFAULT_PASSWORD)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
+                            String uid = Objects.requireNonNull(task.getResult().getUser()).getUid();
+                            createUserInDatabase(uid, username);
                             transitionToSocialMediaActivity();
                         } else {
                             // If sign in fails, display a message to the user.
@@ -90,8 +105,48 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     //transit to city category activity if sign in successfully
-    private void transitionToSocialMediaActivity () {
+    private void transitionToSocialMediaActivity() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
+    }
+
+    private static String wrapWithEmailSuffix(String username) {
+        return username + DEFAULT_EMAIL_SUFFIX;
+    }
+
+    private void createUserInDatabase(String uid, String username) {
+        mMessageing.getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        DatabaseReference userRef = mDatabase.getReference(DATABASE_USERS).child(uid);
+                        userRef.child(DATABASE_USERNAME).setValue(username);
+                        userRef.child(DATABASE_FCMToken).setValue(token);
+                    }
+                });
+    }
+
+    private void updateUserTokenInDatabase(String uid, String username) {
+        mMessageing.getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        mDatabase.getReference(DATABASE_USERS).child(uid).child(DATABASE_FCMToken).setValue(token);
+                    }
+                });
     }
 }
