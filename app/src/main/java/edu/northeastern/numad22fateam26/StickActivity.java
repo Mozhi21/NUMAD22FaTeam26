3a243f857e61896873b1d6203df363c5363c25a8
mozhi21 <mozhi21@gmail.com>
2022-11-06 01:22:29 -0800

    adding userNamedispaly()

--- app/src/main/java/edu/northeastern/numad22fateam26/StickActivity.java
+++ app/src/main/java/edu/northeastern/numad22fateam26/StickActivity.java
@@ -1,11 +1,13 @@
 package edu.northeastern.numad22fateam26;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -13,7 +15,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -43,13 +48,18 @@ public class StickActivity extends AppCompatActivity implements Dialog.DialogLis
     private Map<String, Sticker> idToSticker;
     private List<Sticker> stickers;
     private RecyclerViewStickerAdapter stickerAdapter;
-
+    private TextView userNameDisplay;
     private static final List<String> STICKER_IDS = List.of("b01", "c01", "g01","l01","m01","s01","x01","y01","z01");
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stick);
+
+        userNameDisplay = findViewById(R.id.textViewUserName1);
+        displayUserName(userNameDisplay);
+
 
         spinner = findViewById(R.id.spinner);
         recyclerViewSticker = findViewById(R.id.recyclerSticker);
@@ -174,4 +184,21 @@ public class StickActivity extends AppCompatActivity implements Dialog.DialogLis
             openDialog(position);
         });
     }
+
+    public void displayUserName(TextView userNameDisplay){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseDatabase mDatabase = FirebaseDatabase.getInstance();
+        mDatabase.getReference("users").child(user.getUid()).child("username").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                if (!task.isSuccessful()){
+                    userNameDisplay.setText("Please Sign In!");
+                }
+                else {
+                    userNameDisplay.setText("Welcome Back " +String.valueOf(task.getResult().getValue()));
+                }
+            }
+        });
+    }
+
 }
