dc1e9a87f9308c41b74204bce8c81ba9d80ebdf9
FentPams <xinyifeng94@gmail.com>
2022-11-19 16:27:14 -0800

    Adds fplogin UI

---
+++ app/src/main/java/edu/northeastern/numad22fateam26/fpLogInActivity.java
@@ -0,0 +1,14 @@
+package edu.northeastern.numad22fateam26;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fpLogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fp_log_in);
+    }
+}
--- app/src/main/java/edu/northeastern/numad22fateam26/fpSplashScreenActivity.java
+++ app/src/main/java/edu/northeastern/numad22fateam26/fpSplashScreenActivity.java
@@ -2,13 +2,67 @@ package edu.northeastern.numad22fateam26;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class fpSplashScreenActivity extends AppCompatActivity {
+    private static int SPLASH_SCREEN = 4000;
+    // variables
+    Animation topAnim, bottomAnim;
+    ImageView logo;
+    TextView slogan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Hide top bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_fp_splash_screen);
+        logo = findViewById(R.id.logo);
+        slogan = findViewById(R.id.slogan);
+
+        // In this case, we set animation into it
+        // Animations
+        topAnim = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+
+        //set animations
+        logo.setAnimation(topAnim);
+        slogan.setAnimation(bottomAnim);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Call next screen
+                //After you end the splashscreen activity, you will be directed to login activity.
+                Intent intent = new Intent(fpSplashScreenActivity.this, fpLogInActivity.class);
+
+                //Attach all the elements to animate in design
+                Pair[] pairs = new Pair[2];
+                pairs[0] = new Pair<View, String>(logo, "logo_image");
+                pairs[1] = new Pair<View, String>(slogan, "logo_text");
+
+                //wrap the call in API level 21 or higher
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(fpSplashScreenActivity.this, pairs);
+                    startActivity(intent, options.toBundle());
+                } else {
+                    startActivity(intent);
+                }
+
+            }
+        }, SPLASH_SCREEN);
+
+
     }
 }
