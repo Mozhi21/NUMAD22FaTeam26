23084fe307f4447c84351f54ceb98793c68e7f33
HaozheZhang0818 <zhang.haozhe1@northeastern.edu>
2022-12-04 00:16:44 -0800

    add preference options on home page

--- app/src/main/java/edu/northeastern/numad22fateam26/finalProject/ExploreActivity.java
+++ app/src/main/java/edu/northeastern/numad22fateam26/finalProject/ExploreActivity.java
@@ -14,6 +14,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
@@ -42,6 +43,7 @@ public class ExploreActivity extends AppCompatActivity {
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    private Button b1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
